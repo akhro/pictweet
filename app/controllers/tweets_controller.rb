70ef11dae323b005af44b3eed70b2c8b2fb51eb2
Akihiro Yoshida <akihiroy@akihiromac.local>
2016-08-17 13:35:03 +0900

    ルート設定した

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,17 @@
 class TweetsController < ApplicationController
   def index
-  @tweets = Tweet.all
+  @tweets = Tweet.all.order("id DESC")
   end
   def new
-    
+
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
   end
 end
